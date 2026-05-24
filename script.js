ed4d4e29873a7f60fe57bd7fdc8e90b22ebe6d3a
Cheremsha44 <r.chulkov44@gmail.com>
2026-05-24 13:06:57 +0500

    1st part witchout choise lenght scale

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector("#container")
-container.setAttribute("style", "display: flex; flex-wrap: wrap; background-color: rgba(81, 129, 177, 0.6); width: 817px; height: 816px; margin: 0 auto;")
+container.setAttribute("style", "display: flex; flex-wrap: wrap; background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
 
 for(let i = 0; i < 256; i++){
     let malDiv = document.createElement("div");
