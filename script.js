2f1a6a18c7c97c8949fb70c1aae70ca84ff9273d
Cheremsha44 <r.chulkov44@gmail.com>
2026-05-24 12:04:19 +0500

    16 maldiv in column

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const container = document.querySelector("#container")
 container.setAttribute("style", "background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
 
+for(let i = 0; i < 16; i++){
+    let malDiv = document.createElement("div");
+    malDiv.classList.add("child");
+    container.append(malDiv);
+}
