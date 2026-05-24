ad2d0237da2c367197490a42201aaf85b346ec1a
Cheremsha44 <r.chulkov44@gmail.com>
2026-05-24 11:44:20 +0500

    make big div fix

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const container = document.querySelector("#container")
+container.setAttribute("style", "background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
 
-container.setAttribute("style", "background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto; padding: 15px 0")
