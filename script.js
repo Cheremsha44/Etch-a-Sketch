ab705d12c9525081fd691274427035ba32725dc8
Cheremsha44 <r.chulkov44@gmail.com>
2026-05-24 13:45:31 +0500

    first part scale

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const container = document.querySelector("#container")
-container.setAttribute("style", "display: flex; flex-wrap: wrap; background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
+const vvod = document.querySelector("#myInput")
+const button = document.querySelector("#myButton")
 
-for(let i = 0; i < 256; i++){
-    let malDiv = document.createElement("div");
-    malDiv.classList.add("child");
-    malDiv.addEventListener('mouseenter', () => {
-        malDiv.style.backgroundColor = 'black';
-    });
+let scale = 0
+
+button.addEventListener('click', function(){
+    scale = vvod.value;
+    console.log(scale)
+    for(let i = 0; i < scale*scale; i++){
+        let malDiv = document.createElement("div");
+        malDiv.classList.add("child");
+        malDiv.addEventListener('mouseenter', () => {
+            malDiv.style.backgroundColor = 'black';
+        });
     container.append(malDiv);
 }
+})
+
+container.setAttribute("style", "display: flex; flex-wrap: wrap; background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
+
+
