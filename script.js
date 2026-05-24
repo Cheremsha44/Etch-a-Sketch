255e963ab26073f7032379f3204296aa55b24de8
Cheremsha44 <r.chulkov44@gmail.com>
2026-05-24 12:13:22 +0500

    256 sketch pustoi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container")
-container.setAttribute("style", "background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
+container.setAttribute("style", "display: flex; flex-wrap: wrap; background-color: rgba(81, 129, 177, 0.6); width: 800px; height: 800px; margin: 0 auto;")
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 256; i++){
     let malDiv = document.createElement("div");
     malDiv.classList.add("child");
     container.append(malDiv);
